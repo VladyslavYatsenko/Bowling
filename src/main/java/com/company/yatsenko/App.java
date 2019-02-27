33a4da47cfb359ca96cb9645c81bfe90e21a740a
Vladyslav Yatsenko <vladyslav_yatsenko@epam.com>
2019-02-27 15:21:26 +0200

    Final Version with little bug

--- src/main/java/com/company/yatsenko/App.java
+++ src/main/java/com/company/yatsenko/App.java
@@ -2,29 +2,53 @@ package com.company.yatsenko;
 
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class App {
     public static void main(String[] args) {
-        int sum=0;
-        String text = "X|7/|9-|X|-8|8/|-6|X|X|X|";
-        String[] frames = text.split("\\|");
-        List<Frame> frameList=new ArrayList<>();
-        for(int i=0;i<frames.length;i++){
-            frameList.add(new Frame(frames[i]));
+        String text = "X|7/|9-|X|-8|8/|-6|X|X|X||81";
+        String[] parsedText = text.split("[|]+");
+        System.out.println(Arrays.toString(parsedText));
+        List<Frame> frames = new ArrayList<>();
+
+        for (int i = 0; i < parsedText.length; i++) {
+
+            frames.add(new Frame(parsedText[i].toCharArray()));
+
         }
-        for(Frame f:frameList){
-            System.out.println(f);
+        for (Frame frame : frames) {
+            System.out.println(frame);
         }
-        for(Frame f:frameList){
-            f.countSum();
-            System.out.println(f.getFramePoints());
+        System.out.println(calcTotalPoint(frames));
+
+    }
+
+    public static int calcTotalPoint(List<Frame> frameList) {
+        int totalPoints = 0;
+        for (int i = 0; i < frameList.size(); i++) {
+            if (frameList.get(i).isStrike()) {
+
+                if(!frameList.get(i+1).isStrike()){
+
+                    totalPoints += frameList.get(i).getFirstThrow() + frameList.get(i + 1).getFirstThrow()+frameList.get(i+1).getSecondThrow();
+
+                }else{
+                    totalPoints += frameList.get(i).getFirstThrow() + frameList.get(i + 1).getFirstThrow()+frameList.get(i+2).getFirstThrow();
+                }
+
+
+            } else if (frameList.get(i).isSpare()) {
+
+                totalPoints += frameList.get(i).getTotalPoints() + frameList.get(i + 1).getFirstThrow();
+
+            } else {
+
+                totalPoints += frameList.get(i).getTotalPoints();
+            }
         }
+        return totalPoints;
 
     }
 }
--- src/main/java/com/company/yatsenko/Frame.java
+++ src/main/java/com/company/yatsenko/Frame.java
@@ -3,49 +3,94 @@ package com.company.yatsenko;
 import java.util.Arrays;
 
 public class Frame {
-    private char[] points=new char[2];
-    private int framePoints;
-    private int bonus;
-    public Frame (String text){
-        if(text.length()==1){
-            points[0]=text.charAt(0);
-            points[1]=' ';
-        }else{
-            points[0]=text.charAt(0);
-            points[1]=text.charAt(1);
+    private boolean strike = false;
+    private boolean spare = false;
+    private int firstThrow;
+    private int secondThrow;
+    private int totalPoints;
+    private final int maxFramePoint=10;
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public Frame(char[] text) {
+        if (text[0] == 'X') {
+            this.strike = true;
+            this.spare = false;
+            this.firstThrow = maxFramePoint;
+            this.secondThrow=0;
+            this.totalPoints=firstThrow;
+        }else if (text[1] == '/') {
+            this.strike = false;
+            this.spare = true;
+            this.firstThrow = Character.getNumericValue(text[0]);
+            this.secondThrow=maxFramePoint-Character.getNumericValue(text[0]);
+            this.totalPoints = firstThrow + secondThrow;
+        }
+        else if (text[0] == '-') {
+            this.strike = false;
+            this.spare = false;
+            this.firstThrow = 0;
+            this.secondThrow=Character.getNumericValue(text[1]);
+            this.totalPoints=secondThrow;
+        } else if (text[1] == '-') {
+            this.strike = false;
+            this.spare = false;
+            this.firstThrow = Character.getNumericValue(text[0]);
+            this.secondThrow=0;
+            this.totalPoints=firstThrow;
+        }else {
+            this.strike=false;
+            this.spare=false;
+            this.firstThrow=Character.getNumericValue(text[0]);
+            this.secondThrow=Character.getNumericValue(text[1]);
         }
     }
+
+
+//    public void createFrame(String[] text){
+//        if(text[0].equals("X")){
+//            new Frame().setStrike(true).setSpare(false).setPoints(10);
+//        }
+//        if(text[0].equals("-")){
+//            new Frame().setStrike(false).setSpare(false).setPoints(Integer.parseInt(text[1]));
+//        }
+//        if(text[1].equals("-")){
+//            new Frame().setStrike(false).setSpare(false).setPoints(Integer.parseInt(text[0]));
+//        }
+//        if(text[1].equals("/")){
+//            new Frame().setStrike(false).setSpare(true).setPoints(Integer.parseInt(text[0]));
+//        }
+//    }
+
 
     @Override
     public String toString() {
-        return "Frame{" +
-                "points=" + Arrays.toString(points) +
-                '}';
+        return "Frame->(" +
+                "strike=" + strike +
+                ", spare=" + spare +
+                ", firstThrow=" + firstThrow +
+                ", secondThrow=" + secondThrow +
+                ')';
     }
 
-    public int getBonus() {
-        return bonus;
-    }
-    public void checkUnique(){
-        if(points[0]=='X'){
-            bonus=2;
-        }
-        else if(points[1]=='/'){
-            bonus=1;
-        }
+    public boolean isStrike() {
+        return strike;
     }
 
-    public int getFramePoints() {
-        return framePoints;
+    public boolean isSpare() {
+        return spare;
     }
 
-    public void countSum(){
-        if(points[0]=='X' || points[1]=='/'){
-            framePoints=10;
-        }else if(points[1]!='-'){
-            framePoints=(int) points[0];
-        }else{
-            framePoints=(int) points[1];
-        }
+
+    public int getFirstThrow() {
+        return firstThrow;
     }
+
+    public int getSecondThrow() {
+        return secondThrow;
+    }
+
+
 }
